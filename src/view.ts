2ff7d6c6477a5b35b27210d2bd756a4885b7d472
Leonard Brünings <leonard.bruenings@gradle.com>
2023-12-15 17:23:51 +0100

    Proper path highlighting

--- src/view.ts
+++ src/view.ts
@@ -1,6 +1,15 @@
 import cytoscape, { ElementDefinition } from 'cytoscape';
 import Graph from './Graph';
 
+const colorPalette = [
+    '#00ffff',
+    '#00ff00',
+    '#ffff00',
+    '#ff8000',
+    '#ff0000'
+];
+const maxWeight = 5;
+
 const container: HTMLElement = document.getElementById('cy')!;
 const generateGraphButton: HTMLElement = document.getElementById('generateGraph')!;
 const graphStyle = [
@@ -8,7 +17,14 @@ const graphStyle = [
         selector: 'node',
         style: {
             'background-color': '#666',
-            'label': 'data(id)'
+            'label': 'data(label)'
+        }
+    },
+    {
+        selector: 'node.on-path',
+        style: {
+            'background-color': '#0d6efd',
+            'label': 'data(label)'
         }
     },
     {
@@ -19,7 +35,31 @@ const graphStyle = [
             'target-arrow-color': '#ccc',
             'target-arrow-shape': 'triangle',
             'curve-style': 'bezier',
-            'label': 'data(weight)'
+            'label': 'data(weight)',
+            'opacity': 0.1
+        }
+    },
+    {
+        selector: 'edge.from-selected',
+        style: {
+            'width': 5,
+            'target-arrow-shape': 'triangle',
+            'curve-style': 'bezier',
+            'label': 'data(weight)',
+            'z-index': 9999,
+            'opacity': 1
+        }
+    },
+    {
+        selector: 'edge.on-path',
+        style: {
+            'width': 3,
+            'line-color': '#0d6efd',
+            'target-arrow-color': '#0d6efd',
+            'target-arrow-shape': 'triangle',
+            'curve-style': 'bezier',
+            'label': 'data(weight)',
+            'opacity': 0.75
         }
     }
 ]
@@ -62,19 +102,94 @@ function createGraph(graph: Graph) {
         console.log('tapped ' + node.id());
         console.log(node.data());
         if (selected.indexOf(node.id()) === -1) {
+            if (selected.length > 0) {
+                const hasValidConnection = node.incomers(`edge[source="${selected[selected.length - 1]}"]`).length > 0;
+                if (!hasValidConnection) {
+                    return;
+                }
+                // Hide all outgoing edges from the previously selected node
+                hideOutgoingEdges(selected[selected.length - 1]);
+            }
+
             selected.push(node.id());
-            node.style('background-color', '#0d6efd');
+            node.addClass('on-path');
             hideIncommingEdgesAndHighlightOutgoingEdges(node.id());
+            highlightPath(selected);
         } else {
-            selected.splice(selected.indexOf(node.id()), 1);
-            node.style('background-color', '#666');
+            removeNodeFromPath(node.id());
+            highlightPath(selected);
         }
     });
+    
+    function removeNodeFromPath(nodeId: string) {
+        const index = selected.indexOf(nodeId);
+        if (index !== -1) {
+            const removedNodes = selected.splice(index);
+            removedNodes.forEach((removedNode) => {
+                const node = cy.getElementById(removedNode);
+                node.removeClass('on-path');
+                showIncommingEdgesAndRemoveOutgoingEdges(removedNode);
+            });
+        }
+        if (selected.length > 0) {
+            const previousNode = selected[selected.length - 1];
+            highlightOutgoingEdges(previousNode, true);
+        }
+    }
+
+    function showIncommingEdgesAndRemoveOutgoingEdges(nodeId: string) {
+        resetIncommingEdges(nodeId);
+        resetOutgoingEdges(nodeId);
+    }
+
+    function resetIncommingEdges(nodeId: string) {
+        const node = cy.getElementById(nodeId);
+        const incommingEdges = node.incomers('edge');
+        incommingEdges.removeStyle('display');
+        incommingEdges.removeStyle('line-color');
+        incommingEdges.removeStyle('target-arrow-color');
+        incommingEdges.removeClass('on-path');
+        incommingEdges.removeClass('from-selected');
+    }
+
+    function resetOutgoingEdges(nodeId: string) {
+        const node = cy.getElementById(nodeId);
+        const outgoingEdges = node.outgoers('edge');
+        outgoingEdges.removeStyle('display');
+        outgoingEdges.removeStyle('line-color');
+        outgoingEdges.removeStyle('target-arrow-color');
+        outgoingEdges.removeClass('on-path');
+        outgoingEdges.removeClass('from-selected');
+    }
+    
+
+    function highlightPath(selected: string[]) {
+        const filter = generatePairwiseEntries(selected)
+        for (const filterEntry of filter) {
+            const pathEdges = cy.edges(filterEntry);
+            pathEdges.removeStyle('display');
+            pathEdges.addClass('on-path');
+        }
+    }
+
+    function generatePairwiseEntries(selected: string[]): string[] {
+        const entries: string[] = [];
+
+        for (let i = 0; i < selected.length - 1; i++) {
+            const entry = `[source="${selected[i]}"][target="${selected[i + 1]}"]`;
+            entries.push(entry);
+        }
+
+        return entries;
+    }
 
     function hideOutgoingEdges(nodeId: string) {
         const previousNode = cy.getElementById(nodeId);
         const outgoingEdges = previousNode.outgoers('edge');
         outgoingEdges.style('display', 'none');
+        outgoingEdges.removeClass('from-selected');
+        outgoingEdges.removeStyle('line-color');
+        outgoingEdges.removeStyle('target-arrow-color');
     }
 
     function hideIncommingEdges(nodeId: string) {
@@ -83,11 +198,25 @@ function createGraph(graph: Graph) {
         incommingEdges.style('display', 'none');
     }
 
-    function highlightOutgoingEdges(nodeId: string) {
+    function highlightOutgoingEdges(nodeId: string, unhide: boolean = false) {
         const node = cy.getElementById(nodeId);
         const outgoingEdges = node.outgoers('edge');
-        outgoingEdges.style('line-color', '#0d6efd');
-        outgoingEdges.style('target-arrow-color', '#0d6efd');
+
+        outgoingEdges.forEach(edge => {
+            const weight = edge.data('weight');
+
+            if (weight >= 1) {
+                const colorIndex = Math.floor((weight - 1) / (maxWeight - 1) * (colorPalette.length - 1));
+                const lineColor = colorPalette[colorIndex];
+
+                if (unhide) {
+                    edge.removeStyle('display');
+                }
+                edge.style('line-color', lineColor);
+                edge.style('target-arrow-color', lineColor);
+                edge.addClass('from-selected');
+            }
+        });
     }
 
     function hideIncommingEdgesAndHighlightOutgoingEdges(nodeId: string) {
